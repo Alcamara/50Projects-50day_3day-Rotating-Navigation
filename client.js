a9d3a7f3a3220b802d6498d8523bdb63a1058e44
Al Camara <Alhousseinec@gmail.com>
2022-08-24 18:27:57 -0500

    added css and jquery click event for hamburger button

--- client.js
+++ client.js
@@ -2,4 +2,16 @@ $(document).ready(readyNow)
 
 function readyNow() {
     console.log('test');
+
+    $('#open').on('click', clickHamburger).on('click', menuEvent)
+}
+
+function clickHamburger() {
+    console.log('click burger');
+    $('.container').addClass('show-nav')
+}
+
+function menuEvent() {
+
+    
 }
